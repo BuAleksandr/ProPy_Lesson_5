4c1a529d3c82e77a4c4ae84d0bee465966cdb210
BuAleksandr <87057144+BuAleksandr@users.noreply.github.com>
2021-10-27 00:47:33 +1100

    ProPy_lesson_5

---
+++ Decorators.py
@@ -0,0 +1,55 @@
+import datetime
+import os
+import json
+from pprint import pprint
+import hashlib
+
+
+def logger(old_func):
+    def func(*args, **kwargs):
+        with open("notepad.txt", "r", encoding="utf-8") as f:
+            result_func = old_func(*args, **kwargs)
+            with open("notepad.txt", "w", encoding="utf-8") as f:
+                file_txt = os.path.join(os.getcwd(), "notepad.txt")
+                now = datetime.datetime.now()
+                f.write(f'Путь к файлу с логами {file_txt}\n')
+                f.write(f'{str(now)}\n')
+                f.write(f"Вызвана функция {old_func.__name__}\n")
+                f.write(f"с аргументами {args} {kwargs}\n")
+            return result_func
+
+    return func
+
+
+@logger
+class Country_Iterator:
+    def __init__(self, json_file):
+        with open(file_json, 'r') as file:
+            self.my_json = json.load(file)
+        self.start = -1
+        self.end = len(self.my_json)
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        self.start += 1
+        if self.start == self.end:
+            raise StopIteration
+        return self.my_json[self.start]['name']['common']
+
+
+if __name__ == '__main__':
+    def generate(path):
+        with open(path) as file:
+            for line in file:
+                yield line.upper()
+
+
+    url = "https://en.wikipedia.org/wiki/"
+    file_json = os.path.join(os.getcwd(), "countries.json")
+    for name_country in Country_Iterator(file_json):
+        name_fix = url + name_country.replace(" ", "_")
+        pprint(f'Город {name_country} - {name_fix}')
+        hash_object = hashlib.md5(b'{name_fix}')
+        print(hash_object.hexdigest())
